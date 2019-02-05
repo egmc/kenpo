483464151b03e3ee0b7b90fbfa26c84e2abf19a7
egmc <waltz4debby@eg2mix.com>
2019-02-05 13:14:45 +0900

    add hook sample

--- kenpo.sh
+++ kenpo.sh
@@ -2,11 +2,17 @@
 current=$(cd $(dirname $0);pwd)
 
 for key in `cat $current/list`; do
+  if [ `echo $key | egrep '^#'` ];then
+    #echo 'skip'
+	continue
+  fi
   page_id=`echo $key|cut -f1 -d '_'`
   target_date=`echo $key|cut -f2 -d '_'`
   result_path=$current/result/$key
 
-  curl -s https://as.its-kenpo.or.jp/apply/empty_new?s=$page_id |grep -qs "$target_date"
+  sleep $(($RANDOM % 1000))
+
+  curl -H 'User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1)' -s https://as.its-kenpo.or.jp/apply/empty_new?s=$page_id |grep -qs "$target_date"
   result=$?
 
   if [ -f "$result_path" ];then
@@ -15,8 +21,8 @@ for key in `cat $current/list`; do
     #echo "result $result"
     if [ $result -ne $before ] && [ $result -eq 0 ]; then
       #echo "$target_date is empty now"
-      message="$target_date is empty now! https://as.its-kenpo.or.jp/apply/empty_new?s=$page_id"
-      curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"to\": \"u6f27bae0d2ca7dec1ebca39b4f03eac3\", \"text\":\"say $message\"}" https://tkgiqnzhs2.execute-api.ap-northeast-1.amazonaws.com/production > /dev/null
+      MESSAGE="$target_date is empty now! https://as.its-kenpo.or.jp/apply/empty_new?s=$page_id"
+      . $current/hook.sh
     fi
   fi
   echo -n $result  > $result_path
